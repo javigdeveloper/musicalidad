f887bd5c1ac047aaaa40fcd68ab74e62e1ef624c
Javier <javigdeveloper@gmail.com>
2020-02-10 11:24:01 -0500

    migration for teachers table and routes, controller, model and views added

---
+++ app/controllers/teachers_controller.rb
@@ -0,0 +1,35 @@
+class TeachersController < ApplicationController
+  def index
+    @teachers = Teacher.all
+  end
+  def show
+    @teacher = Teacher.find(params[:id])
+  end
+  def new
+    @teacher = Teacher.new
+  end
+  def edit
+    @teacher = Teacher.find(params[:id])
+  end
+  def create
+    teacher = Teacher.create(teacher_params)
+    
+    redirect_to teacher
+  end
+  def update
+    @teacher = Teacher.find(params[:id])
+    @teacher.update(teacher_params)
+    
+    redirect_to @teacher
+  end
+  
+  def destroy
+    Teacher.find(params[:id]).destroy
+    
+    redirect_to teachers_path
+  end
+  private
+  def teacher_params
+    params.require(:teacher).permit(:name, :pic)
+  end
+end  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_171716) do
+ActiveRecord::Schema.define(version: 2020_02_10_154917) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2020_02_09_171716) do
   end
 
   create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
